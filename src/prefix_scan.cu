d3d537eb2c54a15607a766b7bf6708d0f2a9315e
golosio <golosio@unica.it>
2020-01-02 00:33:30 +0100

    Removed obsolete files and functions. Fixed Nx+1 bug in prefix scan

--- src/prefix_scan.cu
+++ src/prefix_scan.cu
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2016 Bruno Golosio
+Copyright (C) 2020 Bruno Golosio
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
@@ -12,10 +12,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <assert.h>
 #include <stdio.h>
-#include "helper_cuda.h"
-//#include "scan_common.h"
 #include "prefix_scan.h"
 #include "scan.cuh"
 
@@ -23,30 +20,15 @@ const uint PrefixScan::AllocSize = 13 * 1048576 / 2;
 
 int PrefixScan::Init()
 {
-  /*
-    printf("Allocating and initializing host arrays...\n");
-    h_Input     = (uint *)malloc(AllocSize * sizeof(uint));
-    h_OutputCPU = (uint *)malloc(AllocSize * sizeof(uint));
-    h_OutputGPU = (uint *)malloc(AllocSize * sizeof(uint));
-
-    printf("Allocating and initializing CUDA arrays...\n");
-    checkCudaErrors(cudaMalloc((void **)&d_Input, AllocSize * sizeof(uint)));
-    checkCudaErrors(cudaMalloc((void **)&d_Output,
-       AllocSize * sizeof(uint)));
-  */
-    printf("Initializing CUDA-C scan...\n\n");
-    //initScan();
-
-    return 0;
+  //printf("Initializing CUDA-C scan...\n\n");
+  //initScan();
+  
+  return 0;
 }
 
 int PrefixScan::Scan(int *d_Output, int *d_Input, int n)
 {
-  checkCudaErrors(cudaDeviceSynchronize());
-
   prefix_scan(d_Output, d_Input, n, true);
-
-  checkCudaErrors(cudaDeviceSynchronize());
 
   return 0;
 }
@@ -54,8 +36,8 @@ int PrefixScan::Scan(int *d_Output, int *d_Input, int n)
 int PrefixScan::Free()
 {
   //closeScan();
-   //checkCudaErrors(cudaFree(d_Output));
-   //checkCudaErrors(cudaFree(d_Input));
-
-   return 0;
+  //gpuErrchk(cudaFree(d_Output));
+  //gpuErrchk(cudaFree(d_Input));
+  
+  return 0;
 }
--- src/prefix_scan.h
+++ src/prefix_scan.h
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2016 Bruno Golosio
+Copyright (C) 2020 Bruno Golosio
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or

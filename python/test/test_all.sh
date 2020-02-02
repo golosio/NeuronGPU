eb1cc357cb701934d3e1ecd81567c59a6bbb4d27
golosio <golosio@unica.it>
2020-02-02 01:45:01 +0100

    Made script for running various tests

---
+++ python/test/test_all.sh
@@ -0,0 +1,18 @@
+pass_str[0]=PASSED
+pass_str[1]="NOT PASSED"
+:>log.txt
+for fn in test_brunel_list.py test_brunel_outdegree.py test_brunel_user_m1.py test_spike_detector.py; do
+    python $fn >>log.txt
+    res=$?
+    echo ${pass_str[$res]}
+done
+for fn in test_brunel_mpi.py test_brunel_outdegree_mpi.py; do
+    mpirun -np 2 python $fn >>log.txt
+    res=$?
+    echo ${pass_str[$res]}
+done
+for fn in connect getarr setvar2 setvar3; do
+    python test_$fn.py > tmp
+    diff -qs tmp log_$fn.txt
+done
+rm tmp

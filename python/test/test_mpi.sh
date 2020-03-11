d67affc009a2fb5920b76de6b46dc7d64e84d51c
Bruno Golosio <golosio@unica.it>
2020-03-11 09:51:20 -0700

    Fixed test_all.sh and test_mpi.sh

--- python/test/test_mpi.sh
+++ python/test/test_mpi.sh
@@ -2,7 +2,12 @@ mpi_pass_str[0]="MPI TEST PASSED"
 mpi_pass_str[1]="MPI TEST NOT PASSED"
 :>log.txt
 for fn in test_brunel_mpi.py test_brunel_outdegree_mpi.py; do
-    mpirun -np 2 python $fn 2>&1 | grep -v dyl >>log.txt
+    mpirun -np 2 python $fn >> log.txt 2>err.txt
     res=$?
+    cat err.txt >> log.txt
+    rm err.txt
+    if [ "$res" -ne "0" ]; then
+        res=1
+    fi
     echo ${mpi_pass_str[$res]}
 done
